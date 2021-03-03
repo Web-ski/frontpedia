379d0981b5b6d077bba5d1f62a6e2b72f5fb5e53
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2021-03-03 22:08:29 +0100

    Add Header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
-    <router-link to="/">Go to Home</router-link>
-    <router-link to="/about">Go to About</router-link>
+    <Header />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Header from "./components/structures/Header";
 export default {
   name: "App",
+  components: {
+    Header,
+  },
 };
 </script>
 
