25f1ec6087136bb10203734995412ed805142147
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2021-03-03 21:50:38 +0100

    Add pages

---
+++ src/components/pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="about">
+    <h1>About the project</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="home">
+    <h1>Frontpedia</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style>
+</style>
