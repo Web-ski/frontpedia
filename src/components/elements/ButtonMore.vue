f1ccb2ff8a309eaff5a4421b7dc6cfd368a3a17a
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2021-03-06 23:52:10 +0100

    Add style

---
+++ src/components/elements/ButtonMore.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="button-more">Więcej</button>
+</template>
+
+<script>
+export default {
+  name: "ButtonMore",
+};
+</script>
+
+<style>
+.button-more {
+  border: none;
+  color: #444;
+  height: 40px;
+  width: 120px;
+  border-radius: 12px;
+  cursor: pointer;
+  -webkit-backdrop-filter: blur(10px);
+  backdrop-filter: blur(10px);
+  background-color: rgba(255, 255, 255, 0.35);
+  box-shadow: 0 2px 10px 0 rgba(70, 7, 107, 0.17);
+  font-family: Lato, sans-serif;
+  border: 2px solid rgba(255, 255, 255, 0.18);
+  transition: 0.2s;
+}
+
+.button-more:hover {
+  box-shadow: 0 0px 0px 0 rgba(70, 7, 107, 0.17);
+}
+</style>
--- src/components/modules/Card.vue
+++ src/components/modules/Card.vue
@@ -2,23 +2,31 @@
   <article class="card">
     <div class="card-header">
       <h3 class="card-title">{{ title }}</h3>
-      <img class="card-image" :src="image" />
+      <img class="card-image" :src="url + image" />
     </div>
-    <ul class="card-body">
-      <li class="card-text" v-for="item in body" :key="item.text">
+    <div class="card-body">
+      <p class="card-text" v-for="item in body" :key="item.text">
         {{ item.text }}
-        {{ item.link }}
-      </li>
-    </ul>
+      </p>
+    </div>
     <div class="card-footer">
-      <router-link class="card-link" to="/about">więcej</router-link>
+      <router-link class="card-link" :to="title">
+        <ButtonMore />
+      </router-link>
     </div>
   </article>
 </template>
 
 <script>
+import ButtonMore from "../elements/ButtonMore";
 export default {
   name: "Card",
+  data() {
+    return { url: "/images/" };
+  },
+  components: {
+    ButtonMore,
+  },
   props: {
     title: String,
     image: String,
@@ -33,24 +41,32 @@ export default {
   flex-direction: column;
   min-height: 100px;
   min-width: 250px;
+  max-width: 300px;
   margin: 40px auto;
   -webkit-backdrop-filter: blur(10px);
   backdrop-filter: blur(10px);
   background-color: rgba(255, 255, 255, 0.25);
-  border-radius: 5px;
+  border-radius: 12px;
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-  border: 1px solid rgba(255, 255, 255, 0.18);
 }
 
 .card-header,
 .card-body,
 .card-footer {
-  padding: 20px;
+  padding: 10px 20px;
+  letter-spacing: 0.5px;
 }
 
 .card-header {
-  border-bottom: solid 1px darkgrey;
-  display: inline-block;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  -webkit-backdrop-filter: blur(10px);
+  backdrop-filter: blur(10px);
+  background-color: rgba(255, 255, 255, 0.55);
+  border-radius: 12px 12px 0 0;
+  border: 2px solid rgba(245, 106, 106, 0.18);
+  border-bottom: none;
 }
 
 .card-title {
@@ -60,7 +76,30 @@ export default {
 }
 
 .card-text {
-  color: #333;
+  color: #111;
   margin: 0;
+  line-height: 150%;
+  font-size: 1.1rem;
+}
+
+.card-image {
+  height: 40px;
+}
+
+.card-link {
+  text-decoration: none;
+  width: min-content;
+  display: block;
+  margin: 0 0 10px auto;
+}
+
+@media screen and (min-width: 670px) {
+  .card-text {
+    font-size: 1.2rem;
+  }
+
+  .card {
+    max-width: 400px;
+  }
 }
 </style>
