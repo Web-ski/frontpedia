483ac418a8ca0b72fb413a7e6d892d914e970cb6
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2021-03-08 23:01:05 +0100

    Add entryBoard elems

--- src/components/structures/EntryBoard.vue
+++ src/components/structures/EntryBoard.vue
@@ -1,7 +1,33 @@
 <template>
-  <div class="entry-board__container">
-    <h1>{{ entry }}</h1>
-    {{ record }}
+  <div class="entry-board">
+    <article class="entry-body">
+      <h1 class="entry-title">{{ record.title }}</h1>
+      <p
+        class="entry-body__text"
+        v-for="paragraph in record.body"
+        :key="paragraph"
+      >
+        {{ paragraph }}
+      </p>
+      <h4>Źródła:</h4>
+      <ul class="entry-sources">
+        <li v-for="source in record.sources" :key="source.link">
+          <a :href="source.link">{{ source.text }}</a>
+        </li>
+      </ul>
+      <ul class="entry-links">
+        <li v-for="link in record.linkEntries" :key="link">
+          <a :href="link">{{ link }}</a>
+        </li>
+      </ul>
+    </article>
+    <aside class="entry-aside">
+      <h2 class="entry-title">{{ record.title }}</h2>
+      <img class="entry-aside__image" :src="url + record.image" />
+      <a class="entry-aside__link" :href="record.aside.link">{{
+        record.aside.text
+      }}</a>
+    </aside>
   </div>
 </template>
 
@@ -10,21 +36,62 @@ import store from "../../api/store";
 
 export default {
   name: "EntryBoard",
+  data() {
+    return { url: "/images/" };
+  },
   props: {
     entry: String,
   },
   computed: {
     record() {
       let vm = this;
-      let record = store.state.allRecords.filter(
+      let choosenRecord = store.state.allRecords.filter(
         (item) => item.title === vm.entry && item
       );
-      console.log(record);
-      return record;
+      console.log(choosenRecord);
+      return choosenRecord[0];
     },
   },
 };
 </script>
 
 <style>
+.entry-board {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  min-height: 100px;
+  min-width: 250px;
+  max-width: 1000px;
+  padding: 20px;
+  margin: 40px auto;
+  -webkit-backdrop-filter: blur(10px);
+  backdrop-filter: blur(10px);
+  background-color: rgba(255, 255, 255, 0.35);
+  border-radius: 12px;
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  color: black;
+}
+
+.entry-aside {
+  display: flex;
+  flex-direction: column;
+  min-height: 100px;
+  min-width: 250px;
+  max-width: 1000px;
+  padding: 20px;
+  margin: 0 0 0 20px;
+  -webkit-backdrop-filter: blur(10px);
+  backdrop-filter: blur(10px);
+  background-color: rgba(255, 255, 255, 0.35);
+  border-radius: 12px;
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  color: black;
+}
+
+.entry-title {
+  font-size: 2rem;
+  margin: 0;
+}
 </style>
